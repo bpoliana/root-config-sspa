00adbfe8459770c2257178320f3ce28ec1f8af7e
bpoliana <bpolianajaber@gmail.com>
2020-06-30 20:06:16 -0300

    Basic root config auto generated

---
+++ src/tw-root-config.ts
@@ -0,0 +1,21 @@
+import { registerApplication, start } from "single-spa";
+
+registerApplication({
+  name: "@single-spa/welcome",
+  app: () =>
+    System.import(
+      "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
+    ),
+  activeWhen: ["/"],
+});
+
+
+// registerApplication({
+//   name: "@tw/navbar",
+//   app: () => System.import("@tw/navbar"),
+//   activeWhen: ["/"]
+// });
+
+start({
+  urlRerouteOnly: true,
+});
