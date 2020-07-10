1c4559208f911a41468ab0edec2e51dfd46b3c32
bpoliana <bpolianajaber@gmail.com>
2020-07-10 17:08:32 -0300

    Register two microfrontends apps, makes its root configs works and add react as dependency (POC Items 1, 2, 3 validated)

--- src/tw-root-config.ts
+++ src/tw-root-config.ts
@@ -1,20 +1,25 @@
 import { registerApplication, start } from "single-spa";
 
+// registerApplication({
+//   name: "@single-spa/welcome",
+//   app: () =>
+//     System.import(
+//       "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
+//     ),
+//   activeWhen: ["/"],
+// });
+
 registerApplication({
-  name: "@single-spa/welcome",
-  app: () =>
-    System.import(
-      "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
-    ),
+  name: "@tw/single-spa-poc",
+  app: () => System.import("http://localhost:8080/tw-single-spa-poc.js"),
   activeWhen: ["/"],
 });
 
-
-// registerApplication({
-//   name: "@tw/navbar",
-//   app: () => System.import("@tw/navbar"),
-//   activeWhen: ["/"]
-// });
+registerApplication({
+  name: "@tw/poc-sspa",
+  app: () => System.import("http://localhost:8081/tw-poc-sspa.js"),
+  activeWhen: ["/"],
+});
 
 start({
   urlRerouteOnly: true,
